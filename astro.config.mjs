7ba70cd5ebae5980cd32ef36b2874e27f52ff6a6
Andrea Cozart-Lundin <andrea@cozyartzmedia.com>
2025-06-27 17:04:13 -0400

    updated fontawesome

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import react from '@astrojs/react';
-
-import react from '@astrojs/react';
-
 export default defineConfig({
+  output: 'static',
   integrations: [tailwind(), react()],
   vite: {
     server: {
